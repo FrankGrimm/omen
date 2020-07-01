cb22d5c9f4095c6f5ef3fd1698cd42eebbe4f9c7
Frank Grimm <git@frankgrimm.net>
2020-07-01 09:07:32 +0200

    quote test data

--- testdata/quotes.sh
+++ testdata/quotes.sh
@@ -4,5 +4,14 @@ set -e
 declare QUOTE_JSON
 QUOTE_JSON="https://raw.githubusercontent.com/mubaris/motivate/master/motivate/data_unique/unique_quotes.json"
 wget "$QUOTE_JSON" -O "quotes.json"
-jq ".data[] | [.quote, .author] | @csv" "quotes.json" > "quotes.csv"
 
+echo 'ID,Quote,Author' > "quotes-2k.csv"
+# shellcheck disable=SC2016
+jq -r '.data | keys[] as $idx | [$idx, .[$idx].quote, .[$idx].author] | @csv' "quotes.json" >> "quotes-2k.csv"
+
+QUOTE_JSON="https://raw.githubusercontent.com/JamesFT/Database-Quotes-JSON/master/quotes.json"
+wget "$QUOTE_JSON" -O "quotes.json"
+
+echo 'ID,Quote,Author' > "quotes-5k.csv"
+# shellcheck disable=SC2016
+jq -r '. | keys[] as $idx | [$idx, .[$idx].quoteText, .[$idx].quoteAuthor] | @csv' "quotes.json" >> "quotes-5k.csv"
