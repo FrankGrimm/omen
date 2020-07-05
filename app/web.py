5375ac44c978173f75b141208f51cbfaa0a14e5e
Frank Grimm <git@frankgrimm.net>
2020-07-05 20:21:15 +0200

    display app version in footer. closes #15

--- app/web.py
+++ app/web.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for, session
 
 import app.lib.config as config
+from app import __version__ as app_version
 
 BASEURI = config.get("base_uri", "/omen") or "/omen"
 flask_app = app = Flask(__name__, static_url_path=BASEURI + "/static")
@@ -53,7 +54,8 @@ def inject_globals():
     return dict(product_name=config.get("product_name", "Annotations"), \
                 is_authenticated=is_authenticated,
                 tasks=annotation_tasks,
-                cur_year=datetime.utcnow().year)
+                cur_year=datetime.utcnow().year,
+                app_version=app_version)
 
 @app.errorhandler(404)
 def page_not_found(error):
