0961a930ca838399783e05d96ab4ca8c15a03786
Frank Grimm <git@frankgrimm.net>
2020-07-14 15:06:14 +0200

    fix database prestart for production build

---
+++ container/prestart.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -e
+
+# this file is automatically run when starting the
+# Dockerfile.prod version of the OMEN build.
+
+# give dependencies a chance to start
+sleep 5;
+
+# make sure the database schema is up to date
+cd /home/omenuser/app/
+echo "prestart: db upgrade"
+flask db upgrade
+echo "prestart complete"
