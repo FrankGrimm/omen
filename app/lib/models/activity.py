35d3aa6569929dd26eb3475862af4c7256088f7a
Frank Grimm <git@frankgrimm.net>
2020-07-19 00:03:26 +0200

    fix foreign key in activity table

--- app/lib/models/activity.py
+++ app/lib/models/activity.py
@@ -1,7 +1,7 @@
 """
 Model for generic activities (e.g. change events, comments).
 """
-from sqlalchemy import Column, Integer, String, desc, func
+from sqlalchemy import Column, Integer, String, desc, func, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import DateTime
 
@@ -15,7 +15,7 @@ class Activity(Base):
 
     event_id = Column(Integer, primary_key=True)
 
-    owner_id = Column(Integer)
+    owner_id = Column(Integer, ForeignKey("users.uid"))
     owner = relationship("User")
 
     created = Column(DateTime(timezone=True), server_default=func.now())
