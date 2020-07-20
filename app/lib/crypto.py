48b90ec312af3419b20aa9c86a085c303f33df14
Frank Grimm <git@frankgrimm.net>
2020-07-20 11:50:48 +0200

    change to explicit keys instead of implicitely using the system pair

--- app/lib/crypto.py
+++ app/lib/crypto.py
@@ -2,8 +2,8 @@
 Cryptographic helpers
 
 ```python
-tk1 = jwt_encode({"foo": "bar", "val": 42})
-tk2 = jwt_decode(tk1)
+tk1 = jwt_encode({"foo": "bar", "val": 42}, private_key)
+tk2 = jwt_decode(tk1, public_key)
 logging.debug("JWTTest\n%s\n%s", tk1,tk2)
 ```
 """
@@ -18,20 +18,30 @@ import jwt
 import app.lib.config as config
 
 
-def jwt_encode(payload):
-    private_key = config.get("jwt_privkey", None)
+def jwt_encode(payload, private_key):
     if private_key is None or private_key.strip() == "":
-        raise Exception("jwt_privkey configuration missing or malformed")
-
+        raise Exception("private key missing or malformed")
     return jwt.encode(payload, private_key, algorithm="RS256").decode("utf-8")
 
 
-def jwt_decode(token):
+def jwt_decode(token, public_key):
+    if public_key is None or public_key.strip() == "":
+        raise Exception("public key missing or malformed")
+    return jwt.decode(token, public_key, algorithm="RS256")
+
+
+def system_private_key():
+    private_key = config.get("jwt_privkey", None)
+    if private_key is None or private_key.strip() == "":
+        raise Exception("jwt_privkey configuration missing or malformed")
+    return private_key
+
+
+def system_public_key():
     public_key = config.get("jwt_pubkey", None)
     if public_key is None or public_key.strip() == "":
         raise Exception("jwt_pubkey configuration missing or malformed")
-
-    return jwt.decode(token, public_key, algorithm="RS256")
+    return public_key
 
 
 def initialize():
@@ -42,7 +52,7 @@ def initialize():
     logging.info("jwt_privkey not set in config, reinitializing crypto")
     # config.store("testkey", "testval")
 
-    private_key, public_key = generate_key()
+    private_key, public_key = generate_keypair()
 
     config.store("jwt_privkey", private_key)
     config.store("jwt_pubkey", public_key)
@@ -50,7 +60,7 @@ def initialize():
     logging.debug("crypto reinitialization done")
 
 
-def generate_key():
+def generate_keypair():
     key = rsa.generate_private_key(
             backend=default_backend(),
             public_exponent=65537,
