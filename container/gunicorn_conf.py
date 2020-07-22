cae783ae0cf31a75e95d9fb074544631172aae08
Frank Grimm <git@frankgrimm.net>
2020-07-22 10:29:23 +0200

    #46 unified gunicorn configuration

--- container/gunicorn_conf.py
+++ container/gunicorn_conf.py
@@ -1,5 +1,56 @@
+import json
+import multiprocessing
+import os
+
 import app.web
 
 
+workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
+web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "80")
+bind_env = os.getenv("BIND", None)
+use_loglevel = os.getenv("LOG_LEVEL", "info")
+if bind_env:
+    use_bind = bind_env
+else:
+    use_bind = "{host}:{port}".format(host=host, port=port)
+
+cores = multiprocessing.cpu_count()
+workers_per_core = float(workers_per_core_str)
+default_web_concurrency = workers_per_core * cores
+if web_concurrency_str:
+    web_concurrency = int(web_concurrency_str)
+    assert web_concurrency > 0
+else:
+    web_concurrency = int(default_web_concurrency)
+
+# Gunicorn config variables
+loglevel = use_loglevel
+workers = web_concurrency
+bind = use_bind
+keepalive = 120
+errorlog = "-"
+
+# For debugging and testing
+log_data = {
+    "loglevel": loglevel,
+    "workers": workers,
+    "bind": bind,
+    # Additional, non-gunicorn variables
+    "workers_per_core": workers_per_core,
+    "host": host,
+    "port": port,
+}
+
+logdir = os.getenv("LOGDIR", os.path.expanduser("~/logs"))
+if os.path.exists(logdir):
+    capture_output = True
+    accesslog = os.path.join(logdir, "access_log.log")
+    errorlog = os.path.join(logdir, "error_log.log")
+
+print(json.dumps(log_data))
+
+# startup hooks
 on_starting = app.web.on_starting
 on_shutdown = app.web.on_shutdown
