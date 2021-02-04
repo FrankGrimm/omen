cefbaed52554548008e490572018a38043a8000f
Frank Grimm <git@frankgrimm.net>
2021-02-04 12:25:00 +0100

    add nl2br template filter and improve task definition template

--- app/routes/templates.py
+++ app/routes/templates.py
@@ -4,9 +4,18 @@ Template engine enhancements
 import re
 
 from markupsafe import Markup
+from jinja2 import evalcontextfilter, escape
 
 from app.web import app, BASEURI, db
+_paragraph_re = re.compile(r'(?:\r\n|\r(?!\n)|\n){2,}')
 
+@app.template_filter(name="nl2br")
+@evalcontextfilter
+def nl2br(eval_ctx, value):
+    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', Markup('<br>\n')) for p in _paragraph_re.split(escape(value)))
+    if eval_ctx.autoescape:
+        result = Markup(result)
+    return result
 
 @app.template_filter(name="highlight")
 def highlight(value, query):
